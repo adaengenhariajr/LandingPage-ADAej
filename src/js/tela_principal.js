46a204f88a4181bd25e7107fc349754275d9cbda
Andrevictor20 <69045848+Andrevictor20@users.noreply.github.com>
2023-09-27 18:07:32 -0300

    final

--- src/js/tela_principal.js
+++ src/js/tela_principal.js
@@ -1,5 +1,5 @@
 const textElement = document.getElementById('changingText');
-const texts = ["Acredite em si, assim obterá sucesso.","Oportunidades não surgem, você as cria", "A persistência supera a resistência.","                                   "];
+const texts = ["Explore nosso site para conhecer nossos projetos","Descubra como podemos transformar suas ideias em realidade", "Venha nos conhecer e saiba mais sobre o que nos move"];
 let index = 0;
 let charIndex = 0;
 
@@ -14,7 +14,7 @@ function typeText() {
         setTimeout(typeText, 75); // Velocidade de digitação
     } else {
         charIndex = 0;
-        setTimeout(eraseText, 1500); // Tempo de pausa antes de apagar o texto
+        setTimeout(eraseText, 1000); // Tempo de pausa antes de apagar o texto
     }
 }
 
@@ -22,11 +22,11 @@ function eraseText() {
     const currentText = textElement.textContent;
     if (currentText.length > 0) {
         textElement.textContent = currentText.slice(0, -1);
-        setTimeout(eraseText, 100); // Velocidade de apagar o texto
+        setTimeout(eraseText, 150); // Velocidade de apagar o texto
     } else {
         textElement.innerHTML = ''; // Limpa o conteúdo
         index = (index + 1) % texts.length;
-        setTimeout(typeText, 1500); // Tempo de pausa antes de digitar o próximo texto
+        setTimeout(typeText, 1000); // Tempo de pausa antes de digitar o próximo texto
     }
 }
 
